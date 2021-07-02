1a0379a1b4dd65f198ffb46520eafb06937db6fe
minaminfo <inamkbmail@gmail.com>
2021-07-02 19:11:21 +0500

    all fields working

--- themes/callvoiptelefonie/assets/js/calculator.js
+++ themes/callvoiptelefonie/assets/js/calculator.js
@@ -498,7 +498,7 @@ function handleSmallForm() {
   elementId.forEach((e) => elements.push(document.getElementById(e)));
   elements.forEach((e) => {
     const c = e.cloneNode(true);
-    c.classList.add("hidden");
+    c.classList.add("invisible", "absolute");
     formSmall.appendChild(c);
   });
 }
